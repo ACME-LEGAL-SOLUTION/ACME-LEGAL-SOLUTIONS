f9081ca9cbf0d5c0b46bb1309ac9d9fbab4178eb
estantrarealty <estantrarealty@gmail.com>
2026-09-09 03:07:09 +0530

    feat(persistence): expose transactional domain operations in composition

--- api/persistence/persistent-application.js
+++ api/persistence/persistent-application.js
@@ -4,6 +4,7 @@ const { createApplicationRuntime } = require("../runtime/application-runtime");
 const { createSqlRepositories } = require("./sql-repository-adapter");
 const { createTransactionalStorage } = require("./transactional-storage");
 const { createTransactionBoundary } = require("./transaction-contract");
+const { createTransactionalOperations } = require("./transactional-operations");
 
 /**
  * Production composition seam: application services receive the SQL-backed
@@ -13,9 +14,10 @@ function createPersistentApplication({ executor, begin, commit, rollback, provid
   const { repositories } = createSqlRepositories({ executor });
   const transaction = createTransactionBoundary({ begin, commit, rollback });
   const storage = createTransactionalStorage({ repositories, transaction });
+  const operations = createTransactionalOperations({ transaction, repositories });
   const application = createApplicationRuntime({ repositories, provider, clock });
 
-  return Object.freeze({ application, repositories, storage, transaction });
+  return Object.freeze({ application, repositories, storage, transaction, operations });
 }
 
 module.exports = { createPersistentApplication };
